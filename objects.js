1aaf9c49e8a13c78715be326f815ae1d93487747
Emily Asch <e.asch@me.com>
2018-07-28 19:57:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,11 @@ function updatePlaylist (playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist (playlist, artistName){
-  delete playlist['artistName'];
+  delete playlist[artistName];
   return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
 }
